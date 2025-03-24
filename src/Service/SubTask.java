d9121045f32051a5ef6cbcd28bd587c5ff3bc101
Rusty Talgaty <leo59158@gmail.com>
2025-03-24 19:32:49 +0500

    Second push all files for test

--- src/Service/SubTask.java
+++ src/Service/SubTask.java
@@ -2,7 +2,6 @@ package Service;
 
 public class SubTask extends Task {
 
-
     public SubTask(int newEpicId, int id, String taskName, String details, Status status) {
         super(id, taskName, details, status);
         setEpicId(newEpicId);
--- src/Service/Task.java
+++ src/Service/Task.java
@@ -1,6 +1,5 @@
 package Service;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
