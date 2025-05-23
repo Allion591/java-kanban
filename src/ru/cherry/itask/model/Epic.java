3547ce29fda1010d4f13aa99b1eb3ba199437bc0
Rusty Talgaty <leo59158@gmail.com>
2025-05-23 15:40:07 +0500

    Добавлена новая расчетная переменная изменен toString, методы изменены с учетом новых полей

--- src/ru/cherry/itask/model/Epic.java
+++ src/ru/cherry/itask/model/Epic.java
@@ -1,10 +1,13 @@
 package ru.cherry.itask.model;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Epic extends Task {
     private final List<Integer> listSaveSubTasksNum;
+
+    private LocalDateTime endTime;
 
 
     public Epic(String taskName, String details) {
@@ -32,23 +35,48 @@ public class Epic extends Task {
         return super.toCsv();
     }
 
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
     @Override
     public Epic copy() {
         Epic copy = new Epic(this.getTaskName(), this.getDetails());
         copy.setID(this.getID());
+        copy.setStartTime(this.getStartTime());
+        copy.setDurationTask(this.getDurationTask());
+        copy.setEndTime(this.getEndTime());
         copy.listSaveSubTasksNum.addAll(this.listSaveSubTasksNum);
         copy.setStatus(this.getStatus());
         return copy;
     }
 
     @Override
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    @Override
     public String toString() {
-        return "Epic{" +
-                "ID=" + getID() +
-                ", taskName='" + getTaskName() + '\'' +
-                ", details='" + getDetails() + '\'' +
-                ", status=" + getStatus() +
-                ", listSaveSubTasksNum=" + listSaveSubTasksNum +
-                '}';
+        if (getStartTime() == null) {
+            return "Epic{" +
+                    "ID=" + getID() +
+                    ", taskName='" + getTaskName() + '\'' +
+                    ", details='" + getDetails() + '\'' +
+                    ", status=" + getStatus() +
+                    ", listSaveSubTasksNum=" + listSaveSubTasksNum +
+                    '}';
+        } else {
+            return "Epic{" +
+                    "ID=" + getID() +
+                    ", taskName='" + getTaskName() + '\'' +
+                    ", details='" + getDetails() + '\'' +
+                    ", status=" + getStatus() +
+                    ", listSaveSubTasksNum=" + listSaveSubTasksNum +
+                    ", startTime='" + getStartTime().format(formatter) + '\'' +
+                    ", duration='" + getDurationTask().toMinutes() + '\'' +
+                    ",  endTime='" + getEndTime().format(formatter) + '\'' +
+                    '}';
+        }
     }
 }
