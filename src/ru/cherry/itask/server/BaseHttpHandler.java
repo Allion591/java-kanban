88b6c886a5fb84de3d7a0bb62f44ad342ce85488
Rusty Talgaty <leo59158@gmail.com>
2025-06-05 17:27:44 +0500

    Методы readText и getGson перенесены в базовый класс, удален лишний заголовок в отправке в методах

--- src/ru/cherry/itask/server/BaseHttpHandler.java
+++ src/ru/cherry/itask/server/BaseHttpHandler.java
@@ -1,10 +1,14 @@
 package ru.cherry.itask.server;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class BaseHttpHandler {
 
@@ -18,7 +22,7 @@ public class BaseHttpHandler {
 
     public void sendNotFound(HttpExchange exchange, String text) throws IOException {
         byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        exchange.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
         exchange.sendResponseHeaders(404, resp.length);
         exchange.getResponseBody().write(resp);
         exchange.close();
@@ -26,7 +30,7 @@ public class BaseHttpHandler {
 
     public void sendHasInteractions(HttpExchange exchange, String text) throws IOException {
         byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        exchange.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
         exchange.sendResponseHeaders(406, resp.length);
         exchange.getResponseBody().write(resp);
         exchange.close();
@@ -34,7 +38,7 @@ public class BaseHttpHandler {
 
     public void sendInternalServerError(HttpExchange exchange, String text) throws IOException {
         byte[] resp = text.getBytes(StandardCharsets.UTF_8);
-        exchange.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        exchange.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
         exchange.sendResponseHeaders(500, resp.length);
         exchange.getResponseBody().write(resp);
         exchange.close();
@@ -47,5 +51,16 @@ public class BaseHttpHandler {
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(responseBytes);
         }
+    }
+
+    public static Gson getGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+        gsonBuilder.registerTypeAdapter(Duration.class, new DurationAdapter());
+        return gsonBuilder.create();
+    }
+
+    String readText(HttpExchange exchange) throws IOException {
+        return new String(exchange.getRequestBody().readAllBytes(), StandardCharsets.UTF_8);
     }
 }
