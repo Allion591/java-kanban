f4b9192e5605102937293bedc1b87438389a987e
Rusty Talgaty <leo59158@gmail.com>
2025-06-05 17:25:56 +0500

    Изменено наследование на RuntimeException

--- src/ru/cherry/itask/exception/NotFoundException.java
+++ src/ru/cherry/itask/exception/NotFoundException.java
@@ -1,6 +1,6 @@
 package ru.cherry.itask.exception;
 
-public class NotFoundException extends Exception {
+public class NotFoundException extends RuntimeException {
     public NotFoundException(final String message) {
         super(message);
     }
